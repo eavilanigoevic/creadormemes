d9b2f1bec8e8a9cf022e8cf97ef4cf85a3564771
eavilanigoevic <eavila_nigoevic@hotmail.com>
2022-03-01 18:22:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [linea1, setLinea1] = useState("Hola mundo");
+  const [linea2, setLinea2] = useState("Hola mundo 2");
+  const onChangeLinea1 = function (e) {
+    alert(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>{" "}
+      <br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" />
+      <br />
+      <input type="text" placeholder="Linea 2" />
+      <br />
+      <button>Exportar</button>
+      <br />
+      <div>
+        <span>{linea1}</span>
+        <br />
+        <span>{linea2}</span>
+        <br />
+        <img src="" />
+      </div>
     </div>
   );
 }
